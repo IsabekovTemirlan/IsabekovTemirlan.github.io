bbd333a77bcccf96780aad2c1bbb9b57d5e55705
IsabekovTemirlan <isabekovtemirlan@gmail.com>
2021-01-30 20:53:15 +0300

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CopyPlugin = require("copy-webpack-plugin")
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, "/dist"),
+    path: path.join(__dirname, "/docs"),
     filename: "bundle.js"
   },
   module: {
