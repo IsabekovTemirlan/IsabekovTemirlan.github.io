7410801d0703159a19d11a2603ce624d1ccbe6fb
IsabekovTemirlan <isabekovtemirlan@gmail.com>
2021-02-01 16:38:58 +0300

    adaptive design

--- src/scripts/burger.js
+++ src/scripts/burger.js
@@ -1,3 +1,4 @@
 const burgerToggle = document.getElementById('burger')
 const links = document.querySelector('.navbar__links')
 burgerToggle.onclick = () => links.style.display === "none" || links.style.display === "" ? links.style.display = 'initial' : links.style.display = 'none'
+links.onclick = e => links.style.display = "none"
