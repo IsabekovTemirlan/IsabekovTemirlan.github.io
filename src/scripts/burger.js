3e801de01f2e18bb45cc40310392f698ad125314
IsabekovTemirlan <isabekovtemirlan@gmail.com>
2021-02-08 18:07:11 +0300

    add fire to bottom of site

--- src/scripts/burger.js
+++ src/scripts/burger.js
@@ -1,4 +1,8 @@
 const burgerToggle = document.getElementById('burger')
 const links = document.querySelector('.navbar__links')
-burgerToggle.onclick = () => links.style.display === "none" || links.style.display === "" ? links.style.display = 'initial' : links.style.display = 'none'
-links.onclick = e => links.style.display = "none"
+
+if (document.body.clientWidth <= 560) {
+  burgerToggle.onclick = () => links.style.display === "none" || links.style.display === "" ? links.style.display = 'initial' : links.style.display = 'none'
+  links.onclick = e => links.style.display = "none"
+}
+
